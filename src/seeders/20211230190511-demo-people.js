c24aa0f81044d99e3a061637054e7bcc141ceac2
Carlos Henrique Alvarenga dos Santos <carlosho200@hotmail.com>
2021-12-30 16:24:21 -0400

    Creates the People model, People seeders and People migrations

---
+++ src/seeders/20211230190511-demo-people.js
@@ -0,0 +1,64 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    
+    await queryInterface.bulkInsert('People', [
+      {
+				name: 'Ana Souza',
+				status: true,
+				email: 'ana@ana.com',
+				role: 'estudante',
+				createdAt: new Date(),
+				updatedAt: new Date()
+			},
+			{
+				name: 'Marcos Cintra',
+				status: true,
+				email: 'marcos@marcos.com',
+				role: 'estudante',
+				createdAt: new Date(),
+				updatedAt: new Date()
+			},
+			{
+				name: 'Felipe Cardoso',
+				status: true,
+				email: 'felipe@felipe.com',
+				role: 'estudante',
+				createdAt: new Date(),
+				updatedAt: new Date()
+			},
+			{
+				name: 'Sandra Gomes',
+				status: false,
+				email: 'sandra@sandra.com',
+				role: 'estudante',
+				createdAt: new Date(),
+				updatedAt: new Date()
+			},
+			{
+				name: 'Paula Morais',
+				status: true,
+				email: 'paula@paula.com',
+				role: 'docente',
+				createdAt: new Date(),
+				updatedAt: new Date()
+			},
+			{
+				name: 'Sergio Lopes',
+				status: true,
+				email: 'sergio@sergio.com',
+				role: 'docente',
+				createdAt: new Date(),
+				updatedAt: new Date()
+			}      
+    ], {});
+   
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    
+    await queryInterface.bulkDelete('People', null, {});
+    
+  }
+};
